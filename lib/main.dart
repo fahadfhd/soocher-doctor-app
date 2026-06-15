f340869ec98c52b96b56bba1a27e15238bd1828b
shaliq-rahman <shaliqrhmnv@gmail.com>
2026-06-15 16:03:49 +0530

    feat: add Firebase native SDK with google-services.json, set applicationId to com.soocherdoctor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Color(0xFF2E6DD4),
