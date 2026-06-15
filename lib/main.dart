9c1270617bf8d185dff88d478540b4add791ef9a
shaliq-rahman <shaliqrhmnv@gmail.com>
2026-06-15 13:28:20 +0530

    feat: wire up SoocherDoctorApp entry point

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF2E6DD4),
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: Color(0xFF2E6DD4),
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+  runApp(const SoocherDoctorApp());
+}
+
+class SoocherDoctorApp extends StatelessWidget {
+  const SoocherDoctorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Soocher Doctor',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2E6DD4),
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
